c5392ef8c119add3d058199f638dc01a949d1ac8
David Barredo <datdat@rdmsinc.com>
2023-11-17 16:22:16 +0800

    Add project files.

---
+++ MinimalTest/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinimalTest.Models;
+
+namespace MinimalTest.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Server=192.168.1.6;Database=DBName;User ID=dvduser;Password=dvdpassword;Trusted_Connection=True;MultipleActiveResultSets=true;TrustServerCertificate=true");
+        }
+
+        public DbSet<Game> Games => Set<Game>();
+    }
+}
---
+++ MinimalTest/Models/Game.cs
@@ -0,0 +1,12 @@
+﻿namespace MinimalTest.Models
+{
+    public class Game
+    {
+        public int GameID { get; set; }
+        public string Title { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Genre { get; set; }
+        public string Description { get; set; }
+        public bool Active { get; set; }
+    }
+}
